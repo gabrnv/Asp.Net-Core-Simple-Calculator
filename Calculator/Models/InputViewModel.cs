f5df3ccc5738b1dad9a5f73eeef89950d4553bc3
TheChokoGuy <newprogrammer64@gmail.com>
2023-01-27 22:20:09 +0200

    add basic view

---
+++ Calculator/Models/InputViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+{
+    public class InputViewModel
+    {
+        [Required]
+        public string Input { get; set; }
+    }
+}
