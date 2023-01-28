41d169afb52e4fcb6b70d4ce01164fe7b821b3b6
TheChokoGuy <newprogrammer64@gmail.com>
2023-01-28 21:26:42 +0200

    add error handling

--- Calculator/Controllers/HomeController.cs
+++ Calculator/Controllers/HomeController.cs
@@ -23,83 +23,93 @@ namespace Calculator.Controllers
         [HttpPost]
         public IActionResult Calculate(InputViewModel model)
         {
-            string input = model.Input;
-
-            int totalSum = 0;
-
-            List<string> inputs = input.Split().ToList();
-
-            bool isValid = true;
-
-            List<string> signs = new List<string>() {"/", "X"};
-
-            for (int i = 0; i < inputs.Count(); i++)
+            try
             {
-                if (signs.Contains(inputs[i]))
+
+                string input = model.Input;
+
+                int totalSum = 0;
+
+                List<string> inputs = input.Split().ToList();
+
+
+                bool isValid = true;
+
+                List<string> signs = new List<string>() { "/", "X" };
+
+                for (int i = 0; i < inputs.Count(); i++)
                 {
-                    int positionToInsertIn = i - 1;
-                    int previous = int.Parse(inputs[i - 1]);
-                    string sign = inputs[i];
-                    int next = int.Parse(inputs[i + 1]);
+                    if (signs.Contains(inputs[i]))
+                    {
+                        int positionToInsertIn = i - 1;
+                        int previous = int.Parse(inputs[i - 1]);
+                        string sign = inputs[i];
+                        int next = int.Parse(inputs[i + 1]);
+
+                        int sum = 0;
+
+                        switch (sign)
+                        {
+                            case "/":
+                                sum += previous / next;
+                                break;
+                            case "X":
+                                sum += previous * next;
+                                break;
+                        }
+
+                        inputs.RemoveRange(positionToInsertIn, 3);
+                        inputs.Insert(positionToInsertIn, sum.ToString());
+                    }
+                }
+
+                while (isValid)
+                {
+                    string currentInput = inputs[0];
+                    string sign = inputs[1];
+                    string nextInput = inputs[2];
+
+                    int num = int.Parse(currentInput);
+                    int nextNum = int.Parse(nextInput);
 
                     int sum = 0;
 
                     switch (sign)
                     {
+                        case "+":
+                            sum += num + nextNum;
+                            break;
+                        case "-":
+                            sum += num - nextNum;
+                            break;
                         case "/":
-                            sum += previous / next;
+                            sum += num / nextNum;
                             break;
                         case "X":
-                            sum += previous * next;
+                            sum += num * nextNum;
                             break;
                     }
 
-                    inputs.RemoveRange(positionToInsertIn, 3);
-                    inputs.Insert(positionToInsertIn, sum.ToString());
-                }
-            }
+                    inputs.RemoveRange(0, 3);
+                    inputs.Insert(0, sum.ToString());
 
-            while(isValid)
+                    if (inputs.Count() < 3)
+                    {
+                        totalSum += sum;
+                        break;
+                    }
+                }
+                model.Sum = totalSum.ToString() + " ";
+                return View(model);
+
+            }
+            catch (Exception)
             {
-                string currentInput = inputs[0];
-                string sign = inputs[1];
-                string nextInput = inputs[2];
-
-                int num = int.Parse(currentInput);
-                int nextNum = int.Parse(nextInput);
-
-                int sum = 0;
-
-                switch(sign)
-                {
-                    case "+":
-                        sum += num + nextNum;
-                        break;
-                    case "-":
-                        sum += num - nextNum;
-                        break;
-                    case "/":
-                        sum += num / nextNum;
-                        break;
-                    case "X":
-                        sum += num * nextNum;
-                        break;
-                }
-
-                inputs.RemoveRange(0, 3);
-                inputs.Insert(0, sum.ToString());
-
-                if (inputs.Count() < 3)
-                {
-                    totalSum += sum;
-                    break;
-                }
+                model.Sum = "Error";
+                return View(model);
             }
-            
 
-            model.Sum = totalSum.ToString() + " ";
 
-            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
